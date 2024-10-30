69d327409e9890459941d5146e506ff75be64f76
Austin Wright <austwright10@gmail.com>
2024-10-29 20:31:38 -0600

    styled each date

--- date-night/src/app/components/Dashboard.tsx
+++ date-night/src/app/components/Dashboard.tsx
@@ -2,7 +2,7 @@
 import { useEffect } from 'react'
 import { useRouter } from 'next/navigation'
 import { useDateStore } from '@/app/stores/stores'
-import { UserOutlined } from '@ant-design/icons'
+import { UserOutlined, RightOutlined } from '@ant-design/icons'
 
 interface DateItem {
   id: string
@@ -15,8 +15,12 @@ export default function Dashboard() {
 
   const renderItem = (item: DateItem) => (
     <div className='bg-white p-4 my-2 rounded-lg w-full'>
-      <button onClick={() => console.log('Item clicked')}>
-        <p className='text-lg'>{item.title}</p>
+      <button
+        onClick={() => console.log('Item clicked')}
+        className='w-full text-left flex flex-row justify-between items-center'
+      >
+        <p className='text-xl'>{item.title}</p>
+        <RightOutlined />
       </button>
     </div>
   )
@@ -26,7 +30,7 @@ export default function Dashboard() {
   }, [])
 
   return (
-    <div className='flex flex-col items-center p-20 min-h-screen'>
+    <div className='flex flex-col items-center p-10 sm:p-20 min-h-screen'>
       <div className='flex flex-row w-full justify-between items-center mb-6'>
         <h1 className='text-2xl font-semibold text-black'>Previous Dates</h1>
         <button
@@ -38,7 +42,7 @@ export default function Dashboard() {
       </div>
 
       {/* Main Content */}
-      <div className='flex flex-col w-full max-w-xl'>
+      <div className='flex flex-col w-full'>
         {previousDates.length !== 0 ? (
           <div className='space-y-2'>
             {previousDates.map((item: DateItem) => renderItem(item))}
